401001cbd6456d3831fdb07a9d7af2e2237b027c
Martins Ceplis <martins.ceplis@gmail.com>
2019-02-25 06:18:08 +0200

    Join message with string arguments (not JSON)

--- src/server.ts
+++ src/server.ts
@@ -21,8 +21,13 @@ type Client = {
     lastActivity: TimeStamp;
 }
 
+const userNameRegexp = new RegExp(/^[-\w\.\$@\*\!]{1,30}$/);
+export function isValidUsername(userName: string) {
+    return userNameRegexp.test(userName);
+}
+
 export function startServer({ port = 3000, timeOut = 20000 }) {
-    const clients: Client[] = [];
+    let clients: Client[] = [];
     const io = socketIO();
 
     io.on(IncomingSocketEvent.Connection, socket => {
@@ -30,74 +35,89 @@ export function startServer({ port = 3000, timeOut = 20000 }) {
 
         logger.log({ level: "info", message: `Client connected: ${socket.id}` });
 
-        socket.on(IncomingSocketEvent.Join, data => {
+        socket.on(IncomingSocketEvent.Join, userName => {
 
-            try {
-                const joinData = JSON.parse(data);
+            /** Validate user name */
 
-                client = {
-                    name: joinData.name,
-                    socket: socket,
-                    lastActivity: Date.now(),
-                }
+            if (!isValidUsername(userName)) {
+                socket.emit(
+                    OutgoingSocketEvent.Join,
+                    JSON.stringify({ joined: false }),
+                );
+                socket.emit(
+                    OutgoingSocketEvent.Message,
+                    `:${userName} is not valid user name!`,
+                );
+                logger.log({ level: "warn", message: `${userName} is not valid user name!` });
+                return;
+            }
 
-                const existingClient = clients.find(entry => entry.name === joinData.name);
-                if (existingClient) {
-                    socket.emit(
-                        OutgoingSocketEvent.Join,
-                        JSON.stringify({ joined: false }
-                        ),
-                    );
-                    socket.emit(
-                        OutgoingSocketEvent.Message,
-                        `:User with name ${client.name} already exists!`,
-                    );
-                    logger.log({ level: "warn", message: `User with name ${client.name} already exists!` });
+            /** USER name exists */
+
+            const existingClient = clients.find(entry => entry.name === client.name);
+            if (existingClient) {
+                socket.emit(
+                    OutgoingSocketEvent.Join,
+                    JSON.stringify({ joined: false }),
+                );
+                socket.emit(
+                    OutgoingSocketEvent.Message,
+                    `:User with name ${userName} already exists!`,
+                );
+                logger.log({ level: "warn", message: `User with name ${userName} already exists!` });
+                return;
+            }
+
+            /** New USER joined */
+
+            client = {
+                name: userName,
+                socket: socket,
+                lastActivity: Date.now(),
+            };
+
+            clients.push(client);
+            logger.log({ level: "info", message: `User ${client.name} has joined!` });
+
+            client.socket.emit(
+                OutgoingSocketEvent.Join,
+                JSON.stringify(
+                    {
+                        joined: true,
+                        userName: client.name,
+                    }
+                ),
+            );
+            client.socket.emit(
+                OutgoingSocketEvent.Message,
+                `:Welcome to u-chat ${client.name}!`,
+            );
+
+            // broadcast al incoming messages from this client to rest of the clients
+            socket.on(IncomingSocketEvent.Message, (incomingMessage: string) => {
+                const message = `${client.name}:${incomingMessage}`;
+                socket.broadcast.emit(OutgoingSocketEvent.Message, message);
+                logger.log({ level: "verbose", message });
+                client.lastActivity = Date.now();
+            });
+
+            // inactivity check
+            const checkTimeOut = () => {
+                const timeSinceLastActivity = Date.now() - client.lastActivity;
+                if (timeSinceLastActivity > timeOut) {
+                    const message = `:${client.name} has been disconnected due to inactivity`;
+                    socket.broadcast.emit(OutgoingSocketEvent.Message, message);
+                    client.socket.emit(OutgoingSocketEvent.Message, ":You have been disconnected due to inactivity!");
+                    logger.log({ level: "warn", message });
+                    socket.disconnect();
                     return;
                 }
-
-                clients.push(client);
-                logger.log({ level: "info", message: `User ${client.name} has joined!` });
-
-                client.socket.emit(
-                    OutgoingSocketEvent.Join,
-                    JSON.stringify(
-                        {
-                            joined: true,
-                            userName: client.name,
-                        }
-                    ),
-                );
-                client.socket.emit(
-                    OutgoingSocketEvent.Message,
-                    `:Welcome to u-chat ${client.name}!`,
-                );
-
-                socket.on(IncomingSocketEvent.Message, (incomingMessage: string) => {
-                    const message = `${client.name}:${incomingMessage}`;
-                    socket.broadcast.emit(OutgoingSocketEvent.Message, message);
-                    logger.log({ level: "verbose", message });
-                    client.lastActivity = Date.now();
-                });
-
-                const checkTimeOut = () => {
-                    const timeSinceLastActivity = Date.now() - client.lastActivity;
-                    if (timeSinceLastActivity > timeOut) {
-                        const message = `${client.name} has been disconnected due to inactivity`;
-                        socket.broadcast.emit(OutgoingSocketEvent.Message, message);
-                        client.socket.emit(OutgoingSocketEvent.Message, ":You have been disconnected due to inactivity!");
-                        logger.log({ level: "warn", message });
-                        socket.disconnect();
-                        return;
-                    }
-                    setTimeout(checkTimeOut, timeOut);
-                };
-                checkTimeOut();
-            } catch (error) {
-                logger.log({ level: "error", message: "Unexpected format for join message!", error });
-            }
+                setTimeout(checkTimeOut, timeOut);
+            };
+            checkTimeOut();
         });
 
+        // handle disconnect from server
         socket.on(IncomingSocketEvent.Disconnect, () => {
             socket.broadcast.emit(OutgoingSocketEvent.Message, `${client.name} has left`);
             logger.log({ level: "info", message: `User ${client.name} disconnected` });
@@ -107,9 +127,11 @@ export function startServer({ port = 3000, timeOut = 20000 }) {
     const stopServer = (reason: string) => {
         logger.error({ level: "error", message: `Server stopped because of ${reason}` });
         io.close();
+        clients = [];
     }
     process.once("SIGINT", () => stopServer("SIGINT"));
     process.once("SIGTERM", () => stopServer("SIGTERM"));
 
+    logger.log({ level: "info", message: `Server ready. Listening to ${port}` });
     io.listen(port);
 }
